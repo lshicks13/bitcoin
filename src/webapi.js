6807649f3d35f4ed095679897dea396d74b126af
Lauren Hicks <laurenshicks13@gmail.com>
2019-03-15 00:11:38 -0400

    Added new methods

--- src/webapi.js
+++ src/webapi.js
@@ -26,6 +26,32 @@ class WebAPI {
             })
         })
     }
+    getLastBlockNumber() {
+        var url = this.api + this.network;
+
+        return new Promise((resolve, reject) => {
+            request(url, (err,res,body) => {
+                if(err) reject(err);
+                var result = JSON.parse(body)
+                resolve(result.height);
+            })
+        })
+    }
+    getBlock(id) {
+        var url = this.api + this.network + '/blocks/' + id;
+
+        return new Promise((resolve, reject) => {
+            request(url, (err,res,body) => {
+                if(err) reject(err);
+                var result = JSON.parse(body)
+                resolve({
+                    hash: result.hash,
+                    number: result.height,
+                    time: result.time
+                });
+            })
+        })
+    }
 }
 
 module.exports = WebAPI;
